25cccbd86d9e063a66183dbd3c31542da864c9b3
Developer <developer@Creatives-MBP.attlocal.net>
2018-06-07 23:53:19 -0700

    fixing auth bug

--- routes/html.js
+++ routes/html.js
@@ -5,7 +5,6 @@ const htmlController = require("../controller/htmlController");
 module.exports = function(app, passport, db) {
 
     //app.get("/", authController.isLoggedIn, htmlController.root)
-
     //app.get("/dashboard", authController.isLoggedIn, htmlController.showDashboard);
 
 
