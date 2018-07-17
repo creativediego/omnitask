d550f56dcec719541698e46dc24d90dddd4248f1
Diego <23646598+creativediego@users.noreply.github.com>
2018-07-16 21:08:33 -0700

    Replaced Auth0 with Facebook OAuth. Created models for user, goals, and skills. Created controller methods for updating user profile. Updated front end to reflect these backend changes, including error/success message handling.

--- controller/api-controller.js
+++ controller/api-controller.js
@@ -1,50 +1,34 @@
 const db = require("../models");
 
 module.exports.fetchTasks = function(req, res) {
-
-
-    db.Task.findAll({
-        where: { UserId: req.user.id }
-    }).then(function(dbTasks) {
-
-
-        res.json(dbTasks);
-
-
-    });
-
+  db.Task.findAll({
+    where: { UserId: req.user.id }
+  }).then(function(dbTasks) {
+    res.json(dbTasks);
+  });
 };
 
 module.exports.completeTask = function(req, res) {
-
-
-    db.Task.update({
-        completed: true
-
-    }, {
-        where: {
-            id: req.params.id,
-            //UserId: req.user.id
-        }
-
-    }).then(function(dbTask) {
-        res.json(dbTask)
-
-    });
-
-
+  db.Task.update(
+    {
+      completed: true
+    },
+    {
+      where: {
+        id: req.params.id
+        //UserId: req.user.id
+      }
+    }
+  ).then(function(dbTask) {
+    res.json(dbTask);
+  });
 };
 
 module.exports.createTask = function(req, res) {
-
-    db.Task.create({
-            title: req.body.task,
-            UserId: req.user.id
-
-        })
-        .then(function(dbTask) {
-            res.redirect("/dashboard");
-        });
-
-
+  db.Task.create({
+    title: req.body.task,
+    UserId: req.user.id
+  }).then(function(dbTask) {
+    res.redirect("/dashboard");
+  });
 };
